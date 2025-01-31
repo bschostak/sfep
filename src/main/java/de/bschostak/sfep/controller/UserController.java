073c3c30290a4b0bd50db11cb33aab27b3bc3f13
bschostak <bartek_sz02@proton.me>
2025-01-31 19:46:38 +0100

    build(docker-file): Postgress container

--- src/main/java/de/bschostak/sfep/controller/UserController.java
+++ src/main/java/de/bschostak/sfep/controller/UserController.java
@@ -3,11 +3,14 @@ package de.bschostak.sfep.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
     public Logger LOG = LoggerFactory.getLogger(UserController.class);
 
-    @getMapping("/users")
-    public void getUsers() {
-        LOG.info("getUsers");
+    @GetMapping("/users")
+    public String getUsers() {
+        return "LOVE";
     }
 }
